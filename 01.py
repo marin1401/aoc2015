9cf6ac15f5e016a0f2dc34161329603a7d18dcd6
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-24 20:10:41 +0100

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,22 @@
+#Day 01
+
+with open('./01.txt') as myinput:
+    instructions = myinput.read()
+
+#Part 1
+
+floor = instructions.count('(')-instructions.count(')')
+
+print(floor)
+
+#Part 2
+
+current_floor = 0
+for i in range(len(instructions)):
+    if instructions[i] is '(':
+        current_floor += 1
+    else:
+        current_floor -= 1
+    if current_floor == -1:
+        print(i+1)
+        break
