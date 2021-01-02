d748dea29b5e79e1829e295e058e43939f8465f9
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-02 20:06:20 +0100

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,23 @@
+#Day 04
+
+import hashlib
+
+with open('./04.txt') as myinput:
+    key = myinput.read()
+
+def find_hash(key, leading_numbers):
+    result, i = 0, 0
+    while not result:
+        if hashlib.md5(f'{key}{i}'.encode()).hexdigest()[:len(leading_numbers)] == leading_numbers:
+            result = i
+        else:
+            i += 1
+    return result
+
+#Part 1
+
+print(find_hash(key, '00000'))
+
+#Part 2
+
+print(find_hash(key, '000000'))        
