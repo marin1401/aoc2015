0b5b937d0c7ad49f243284934ecdbf1bab10586c
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-21 14:21:24 +0100

    Update 06.py

--- 06.py
+++ 06.py
@@ -3,38 +3,32 @@
 with open('./06.txt') as myinput:
     instructions = myinput.readlines()
 
-instructions = [i.replace('n o', 'n_o').split() for i in instructions]
+instructions = [i.split() for i in instructions]
 
-lights = [[0 for i in range(1000)] for j in range(1000)]
+grid_size = 1000
+lights = [[0 for x in range(grid_size)] for y in range(grid_size)]
+brightness = [[0 for x in range(grid_size)] for y in range(grid_size)]
 
-def switch(instructions, lights, part):
-    for instruction in instructions:
-        do = instruction[0]
-        [x1, y1] = list(map(int, instruction[1].split(',')))
-        [x2, y2] = list(map(int, instruction[3].split(',')))
-        for x in range(x2 - x1 + 1):
-            for y in range(y2 - y1 + 1):
-                if do == 'turn_on':
-                    lights[x1 + x][y1 + y] = 1 if part == 1 else lights[x1 + x][y1 + y] + 1
-                elif do == 'turn_off':
-                    if lights[x1 + x][y1 + y]:
-                        lights[x1 + x][y1 + y] = 0 if part == 1 else lights[x1 + x][y1 + y] - 1
-                elif do == 'toggle':
-                    if part == 1:
-                        lights[x1 + x][y1 + y] = 0 if lights[x1 + x][y1 + y] == 1 else 1
-                    else:
-                        lights[x1 + x][y1 + y] += 2
+for instruction in instructions:
+    do = instruction[1]
+    x1, y1 = map(int, instruction[-3].split(','))
+    x2, y2 = map(int, instruction[-1].split(','))
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            if do == 'on':
+                lights[x][y] = 1
+                brightness[x][y] += 1
+            elif do == 'off':
+                lights[x][y] = 0
+                brightness[x][y] -= 1 if brightness[x][y] else 0
+            else:
+                lights[x][y] = 0 if lights[x][y] else 1
+                brightness[x][y] += 2
 
 #Part 1
-
-switch(instructions, lights, 1)
 
 print(sum(map(sum, lights)))
 
 #Part 2
 
-lights = [[0 for i in range(1000)] for j in range(1000)]
-
-switch(instructions, lights, 2)
-
-print(sum(map(sum, lights)))
+print(sum(map(sum, brightness)))
