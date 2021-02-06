947a4995a2b134c04d40f1378d0ca3079c5046f1
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-02-06 14:27:07 +0100

    Update 08.py

--- 08.py
+++ 08.py
@@ -3,4 +3,10 @@
 with open('./08.txt') as myinput:
     inputlines = myinput.readlines()
 
+#Part 1
+
 print(sum(len(line.strip()) - len(eval(line.strip())) for line in inputlines))
+
+#Part 2
+
+print(sum(2 + line.strip().count('"') + line.strip().count('\\') for line in inputlines))
